642b500142e07a7fad144a5bbc810c4962087d59
bvenceslas <bvenceslas@gmail.com>
2022-10-13 16:46:19 +0200

    ussd code

--- index.js
+++ index.js
@@ -4,11 +4,60 @@ require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 3333;
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 // code here
-app.use('/', async (req, res) => {
-    res.json('MediCheckup responding successfully ...')
-})
+// app.use('/', async (req, res) => {
+//     res.json('MediCheckup responding successfully ...')
+// })
+
+
+app.post('/ussd', (req, res) => {
+    // Read the variables sent via POST from our API
+    const {
+        sessionId,
+        serviceCode,
+        phoneNumber,
+        text,
+    } = req.body;
+
+    let response = '';
+
+    if (text == '') {
+        // This is the first request. Note how we start the response with CON
+        response = `CON What would you like to check
+        1. My account
+        2. My phone number`;
+    } else if (text == '1') {
+        // Business logic for first level response
+        response = `CON Choose account information you want to view
+        1. Account number`;
+    } else if (text == '2') {
+        // Business logic for first level response
+        // This is a terminal request. Note how we start the response with END
+        response = `END Your phone number is ${phoneNumber}`;
+    } else if (text == '1*1') {
+        // This is a second level response where the user selected 1 in the first instance
+        const accountNumber = 'ACC100101';
+        // This is a terminal request. Note how we start the response with END
+        response = `END Your account number is ${accountNumber}`;
+    }
+
+    // Send the response back to the API
+    res.set('Content-Type: text/plain');
+    res.send(response);
+});
+
 
 app.listen(port, () => {
     console.log(`App listening on ${port}`);
 })
+
+
+
+
+
+
+
