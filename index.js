556f2fc6b99a7ec3c578ff63c053d0a5286070b8
bvenceslas <bvenceslas@gmail.com>
2022-10-13 17:46:56 +0200

    Add new com to codebase

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.post('/ussd', (req, res) => {
         1. Yes
         2. No`;
         // response = `END Your phone number is ${phoneNumber}`;
-        if (text == '1') {
+        if (text == '2*1') {
             // This is a second level response where the user selected 1 in the first instance
             // const accountNumber = 'ACC100101';
             // This is a terminal request. Note how we start the response with END
@@ -52,35 +52,26 @@ app.post('/ussd', (req, res) => {
             1. Yes
             2. No`;
 
-            if (text == '1') {
+            if (text == '2*1*1') {
 
                 response = `CON Do you have muscle and joint pain?.
                 1. Yes
                 2. No`;
 
-                if (text == '1') {
+                if (text == '2*1*1*1') {
 
                     response = `CON Do you have Sore throat?.
                     1. Yes
                     2. No`;
 
-                    if (text == '1') {
+                    if (text == '2*1*1*1*1') {
 
                         response = `END I am sorry to announce you that, 
                         these look like the EBOLA signs and symptoms.
                         Please contact this phone number for an emergency physical support`;
                     }
-
-
-
                 }
-
-
-
             }
-
-
-
             response = `CON How are feeling`;
         }
     }
