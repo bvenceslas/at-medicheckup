b19be3dd9684bbdefc9f9beabb5da796b9bce244
bvenceslas <bvenceslas@gmail.com>
2024-04-10 09:09:09 +0200

    add endpoint to test API

--- index.js
+++ index.js
@@ -1,102 +1,73 @@
-const express = require('express');
-require('dotenv').config()
+const express = require("express");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3333;
 
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post('/ussd', (req, res) => {
-    const {
-        sessionId,
-        serviceCode,
-        phoneNumber,
-        text,
-    } = req.body;
+app.get("/test", (req, res) => {
+  res.json({ msg: "API working properly ..." });
+});
 
-    let response = '';
+app.post("/ussd", (req, res) => {
+  const { sessionId, serviceCode, phoneNumber, text } = req.body;
 
-    if (text == '') {
-        response = `CON Welcome to MediCheckup.
+  let response = "";
+
+  if (text == "") {
+    response = `CON Welcome to MediCheckup.
         Murakaze neza, Kuri MediCheckup.
         
         Please Choose a language
         1. Kinyarwanda
         2. English`;
-    } else if (text == '1') {
-        response = `END Mutwihanganire ntago gukoresha ikinyarwanda birakunda!!`;
-    } else if (text == '2') {
-        response = `CON Do you have fever?.
+  } else if (text == "1") {
+    response = `END Mutwihanganire ntago gukoresha ikinyarwanda birakunda!!`;
+  } else if (text == "2") {
+    response = `CON Do you have fever?.
         1. Yes
         2. No`;
-
-    } else if (text == '2*1') {
-        response = `CON Do you have headache?.
+  } else if (text == "2*1") {
+    response = `CON Do you have headache?.
         1. Yes
         2. No`;
-
-    } else if (text == '2*2') {
-        response = `CON Do you have headache?.
+  } else if (text == "2*2") {
+    response = `CON Do you have headache?.
         1. Yes
         2. No`;
-    }
-    else if (text == '2*1*1') {
-
-        response = `CON Do you have muscle and joint pain?.
+  } else if (text == "2*1*1") {
+    response = `CON Do you have muscle and joint pain?.
         1. Yes
         2. No`;
-
-
-    } else if (text == '2*2*1') {
-
-        response = `CON Do you have muscle and joint pain?.
+  } else if (text == "2*2*1") {
+    response = `CON Do you have muscle and joint pain?.
         1. Yes
         2. No`;
-
-
-    }
-    else if (text == '2*1*1*1') {
-
-        response = `CON Do you have Sore throat?.
+  } else if (text == "2*1*1*1") {
+    response = `CON Do you have Sore throat?.
         1. Yes
         2. No`;
-
-
-    } else if (text == '2*2*1*1') {
-
-        response = `CON Do you have Sore throat?.
+  } else if (text == "2*2*1*1") {
+    response = `CON Do you have Sore throat?.
         1. Yes
         2. No`;
-
-
-    }
-    else if (text == '2*1*1*1*1') {
-
-        response = `END We are sorry to announce you that, these look like the EBOLA signs and symptoms.
+  } else if (text == "2*1*1*1*1") {
+    response = `END We are sorry to announce you that, these look like the EBOLA signs and symptoms.
         Please contact 007 for an emergency MEDICAL SUPPORT`;
-    }
-    else if (text == '2*2*1*1*1') {
-
-        response = `END We are sorry for the body pain, 
+  } else if (text == "2*2*1*1*1") {
+    response = `END We are sorry for the body pain, 
         these don't look like the EBOLA signs and symptoms.
         Please contact 911 for a MEDICAL ADVICE`;
-    }
+  }
 
-    // Send the response back to the API
-    res.set('Content-Type: text/plain');
-    res.send(response);
+  // Send the response back to the API
+  res.set("Content-Type: text/plain");
+  res.send(response);
 });
 
-
 app.listen(port, () => {
-    console.log(`App listening on ${port}`);
-})
-
-
-
-
-
-
-
+  console.log(`App listening on ${port}`);
+});
