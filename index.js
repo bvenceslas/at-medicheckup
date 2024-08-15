3a3d7c6833ba18246f8e8e1ec1e2ae1fb286a7de
bvenceslas <bvenceslas@gmail.com>
2024-08-15 07:07:07 +0200

    remove firebase hosting configs

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3333;
 
+// app routes
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// add app routes
+// cors
+app.use(cors());
+
+// ussd routes
 const ussdRoutes = require("./routes/ussd.routes");
 
 // routes use
